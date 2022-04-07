ff094b15865066f146d34b4fa00f4c0394c9457a
AdaoBJr <adaobjr@gmail.com>
2022-04-06 22:33:51 -0400

    feat: :sparkles: added textInput lastname on createAccount

--- src/lib/pages/CreateAccount/createAccount.js
+++ src/lib/pages/CreateAccount/createAccount.js
@@ -6,15 +6,23 @@ const CreateAccount = (props) => {
   const classes = useStyle(defaultClasses, props.classes);
 
   return (
-    <>
-      <h1 className={classes.root}>CreateAccount</h1>
-      <InputText
-        label="Primeiro Nome"
-        name="firstname"
-        placeholder="Insira seu primeiro nome"
-        classes={{ input: classes.createAccountInput }}
-      />
-    </>
+    <div className={classes.root}>
+      <div className={classes.wrapper}>
+        <h1 className={classes.title}>CreateAccount</h1>
+        <InputText
+          label="Nome"
+          name="firstname"
+          placeholder="Insira seu nome"
+          classes={{ input: classes.createAccountInput }}
+        />
+        <InputText
+          label="Sobrenome"
+          name="lastname"
+          placeholder="Insira seu sobrenome"
+          classes={{ input: classes.createAccountInput }}
+        />
+      </div>
+    </div>
   );
 };
 
