5962787632dac6e2f52dcc46e75b43ee86b58ae4
AdaoBJr <adaobjr@gmail.com>
2022-04-06 21:17:24 -0400

    feat: :art: updated route resgister -> createAccount

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import SignIn from './lib/pages/SignIn';
 function Router() {
   return (
     <Routes>
-      <Route path="/register" element={<CreateAccount />} />
+      <Route path="/create-account" element={<CreateAccount />} />
       <Route path="/login" element={<SignIn />} />
     </Routes>
   );
