257cef3ebfda0ec07b64d8361926261307c70c65
AdaoBJr <adaobjr@gmail.com>
2022-04-06 21:19:03 -0400

    feat: :art: updated func mergeClasses -> useStyle on SignIn

--- src/lib/pages/SignIn/signIn.js
+++ src/lib/pages/SignIn/signIn.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Button, mergeClasses } from '@goshirts-react/lib';
+import { Button, useStyle } from '@goshirts-react/lib';
 
 import defaultClasses from './signIn.module.css';
 
 const SignIn = (props) => {
-  const { useStyle } = mergeClasses();
   const classes = useStyle(defaultClasses, props.classes);
   return (
     <>
