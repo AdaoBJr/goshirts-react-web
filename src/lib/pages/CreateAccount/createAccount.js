9fd9e58ab043477ce1288a0f7f4b063a34384d9a
AdaoBJr <adaobjr@gmail.com>
2022-04-08 15:29:54 -0400

    feat: :construction: working on CreateAccount Component - added events

--- src/lib/pages/CreateAccount/createAccount.js
+++ src/lib/pages/CreateAccount/createAccount.js
@@ -1,6 +1,9 @@
 import React from 'react';
+
 import defaultClasses from './createAccount.module.css';
 import { InputText, Dropdown, useStyle } from '@goshirts-react/lib';
+
+import { useCreateAccount } from '../../services/talons';
 
 const items = [
   { key: 1, label: 'Masculino', value: 0 },
@@ -10,13 +13,34 @@ const items = [
 const CreateAccount = (props) => {
   const classes = useStyle(defaultClasses, props.classes);
 
+  const { handleChange, onValueChange, inputData } = useCreateAccount();
+
+  console.log(inputData);
+
   return (
     <div className={classes.root}>
       <div className={classes.wrapper}>
         <h1 className={classes.title}>Cadastrar</h1>
-        <InputText id="firstname" name="firstname" label="Nome*" autoComplete="off" />
-        <InputText id="lastname" name="lastname" label="Sobrenome*" autoComplete="off" />
-        <Dropdown label="Gênero*" items={items} />
+        <InputText
+          id="firstname"
+          name="firstname"
+          label="Nome*"
+          autoComplete="off"
+          onBlur={handleChange}
+        />
+        <InputText
+          id="lastname"
+          name="lastname"
+          label="Sobrenome*"
+          autoComplete="off"
+          onBlur={handleChange}
+        />
+        <Dropdown
+          id="gender"
+          label="Gênero*"
+          items={items}
+          onValueChange={onValueChange}
+        />
       </div>
     </div>
   );
