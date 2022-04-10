db7686a613589b7c84acd9fb6ecbfe8e5c829524
AdaoBJr <adaobjr@gmail.com>
2022-04-10 00:40:38 -0400

    feat: :sparkles: created element Form on createAccount

--- src/lib/pages/CreateAccount/createAccount.js
+++ src/lib/pages/CreateAccount/createAccount.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import defaultClasses from './createAccount.module.css';
-import { InputText, Dropdown, useStyle } from '@goshirts-react/lib';
+import { InputText, Dropdown, useStyle, Button } from '@goshirts-react/lib';
 
 import { useCreateAccount } from '../../services/talons';
 
@@ -21,7 +21,7 @@ const country = [
 const CreateAccount = (props) => {
   const classes = useStyle(defaultClasses, props.classes);
 
-  const { handleChange, onValueChange, inputData } = useCreateAccount();
+  const { handleChange, handleSubmit, onValueChange, inputData } = useCreateAccount();
 
   console.log(inputData);
 
@@ -29,105 +29,110 @@ const CreateAccount = (props) => {
     <div className={classes.root}>
       <div className={classes.wrapper}>
         <h1 className={classes.title}>Cadastrar</h1>
-        <InputText
-          id="firstname"
-          type="text"
-          name="firstname"
-          label="Nome*"
-          autoComplete="off"
-          onBlur={handleChange}
-        />
-        <InputText
-          id="lastname"
-          type="text"
-          name="lastname"
-          label="Sobrenome*"
-          autoComplete="off"
-          onBlur={handleChange}
-        />
-        <div className={classes.genderBirth}>
+        <form onSubmit={handleSubmit} className={classes.form}>
+          <InputText
+            id="firstname"
+            type="text"
+            name="firstname"
+            label="Nome*"
+            autoComplete="off"
+            onBlur={handleChange}
+          />
+          <InputText
+            id="lastname"
+            type="text"
+            name="lastname"
+            label="Sobrenome*"
+            autoComplete="off"
+            onBlur={handleChange}
+          />
+          <div className={classes.genderBirth}>
+            <Dropdown
+              id="gender"
+              label="Gênero*"
+              items={gender}
+              onValueChange={onValueChange}
+            />
+            <InputText
+              id="birthOfDate"
+              type="text"
+              name="birthOfDate"
+              label="Data de Nascimento*"
+              autoComplete="off"
+              onBlur={handleChange}
+            />
+          </div>
           <Dropdown
-            id="gender"
-            label="Gênero*"
-            items={gender}
+            id="country"
+            label="País*"
+            items={country}
             onValueChange={onValueChange}
+            classes={{
+              items: classes.dropCountryItems,
+              itemsActive: classes.dropCountryItemsActive,
+            }}
           />
+          <div className={classes.cpfPhone}>
+            <InputText
+              id="CPF"
+              type="text"
+              name="CPF"
+              label="CPF*"
+              autoComplete="off"
+              onBlur={handleChange}
+            />
+            <InputText
+              id="phone"
+              type="text"
+              name="phone"
+              label="Telefone Celular*"
+              autoComplete="off"
+              onBlur={handleChange}
+            />
+          </div>
           <InputText
-            id="birthOfDate"
-            type="text"
-            name="birthOfDate"
-            label="Data de Nascimento*"
+            id="email"
+            type="e-mail"
+            name="email"
+            label="E-mail*"
             autoComplete="off"
             onBlur={handleChange}
           />
-        </div>
-        <Dropdown
-          id="country"
-          label="País*"
-          items={country}
-          onValueChange={onValueChange}
-          classes={{
-            items: classes.dropCountryItems,
-            itemsActive: classes.dropCountryItemsActive,
-          }}
-        />
-        <div className={classes.cpfPhone}>
           <InputText
-            id="CPF"
-            type="text"
-            name="CPF"
-            label="CPF*"
+            id="newsletter"
+            type="checkbox"
+            name="newsletter"
+            label="Quero receber a newsletter"
+            autoComplete="off"
+            classes={{
+              root: classes.checkNewsletter,
+              label: classes.label,
+              input: classes.input,
+              textVisible: classes.textVisible,
+              textInvisible: classes.textInvisible,
+            }}
+            onChange={handleChange}
+          />
+          <InputText
+            id="password"
+            type="password"
+            name="password"
+            label="Senha*"
             autoComplete="off"
             onBlur={handleChange}
           />
           <InputText
-            id="phone"
-            type="text"
-            name="phone"
-            label="Telefone Celular*"
+            id="confirm_password"
+            type="password"
+            name="confirm_password"
+            label="Confirme senha*"
             autoComplete="off"
             onBlur={handleChange}
           />
-        </div>
-        <InputText
-          id="email"
-          type="e-mail"
-          name="email"
-          label="E-mail*"
-          autoComplete="off"
-          onBlur={handleChange}
-        />
-        <InputText
-          id="newsletter"
-          type="checkbox"
-          name="newsletter"
-          label="Quero receber a newsletter"
-          autoComplete="off"
-          classes={{
-            root: classes.checkNewsletter,
-            label: classes.label,
-            input: classes.input,
-            textVisible: classes.textVisible,
-            textInvisible: classes.textInvisible,
-          }}
-          onChange={handleChange}
-        />
-        <InputText
-          id="password"
-          type="password"
-          name="password"
-          label="Senha*"
-          autoComplete="off"
-          onBlur={handleChange}
-        />
-        <InputText
-          id="confirm_password"
-          type="password"
-          name="confirm_password"
-          label="Confirme senha*"
-          autoComplete="off"
-          onBlur={handleChange}
-        />
+          <Button type="submit" id="register" classes={{ button: classes.buttonSubmit }}>
+            Cadastre-se
+          </Button>
+        </form>
       </div>
     </div>
   );
