37765ea92739428028024a28802f46393c9182c0
AdaoBJr <adaobjr@gmail.com>
2022-04-12 22:32:59 -0400

    refactor: :art: updated customer gql

--- src/gql/customer/index.js
+++ src/gql/customer/index.js
@@ -1,7 +1,6 @@
 import { generateMutationOp } from '../../../generated';
 
-export const createCustomerGQL = ({ data }) => {
-  return generateMutationOp({
+export const createCustomerGQL = ({ data }) =>
+  generateMutationOp({
     createCustomer: [{ data }, { customer: { id: true, email: true, firstname: true } }]
   });
-};
