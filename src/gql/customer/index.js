dbfbc2d3b8bdaa00226f3f456de958ed669ede68
AdaoBJr <adaobjr@gmail.com>
2022-04-13 08:53:52 -0400

    feat: :sparkles: created signInCustomerGQL on Customer gql

--- src/gql/customer/index.js
+++ src/gql/customer/index.js
@@ -4,3 +4,8 @@ export const createCustomerGQL = ({ data }) =>
   generateMutationOp({
     createCustomer: [{ data }, { customer: { id: true, email: true, firstname: true } }]
   });
+
+export const signInCustomerGQL = ({ data }) =>
+  generateMutationOp({
+    signInCustomer: [{ data }, { token: true }]
+  });
